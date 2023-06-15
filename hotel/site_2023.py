31d55533275a65b60d864c3fcf5a4ff2fb847bae
Eduardo Medina <eduardo1992me@gmail.com>
2023-06-15 11:57:52 -0600

    Funcionalidad para sacar informacion de los sitios de guru version 2023 logrado

--- hotel/site_2023.py
+++ hotel/site_2023.py
@@ -5,13 +5,29 @@ import time
 
 target_url = "https://hotelcasasantamaria.mx/es/availability?checkout=2023-07-24&currency=MXN&occupancy=2&checkin=2023-07-23"
 
-l=list()
-g=list()
+l=[]
+g=[]
 o={}
 k={}
+
 
 headers={"User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36"}
 
 resp = requests.get(target_url, headers=headers)
 
 soup = BeautifulSoup(resp.text, 'lxml')
+
+h2 = soup.find_all("h2")
+for room in h2:
+    l.append(room.text)
+del l[0]
+# print(l)
+
+p = soup.find_all("p", class_='text-2xl font-bold')
+for price in p:
+    g.append(price.text[4:])
+# print(g)
+
+room_and_price = {l:g for (l,g) in zip(l,g)}
+
+print(room_and_price)
